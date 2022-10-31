1f0c01ac675bcc03d9666309dec1324683cea1fc
valeriad2002 <valeria.dumasilva@upse.edu.ec>
2022-10-31 18:12:05 -0500

    wheater

---
+++ src/assets/imagenes.js
@@ -0,0 +1,22 @@
+import despejado from './imagenes/despejado.jpg';
+import llovizna from './imagenes/llovizna.jpg';
+import lluviadelluvia from'./imagenes/lluvia de lluvia.jpeg';
+import lluvia from'./imagenes/lluvia.jpg';
+import niebla from './imagenes/niebla.jpg';
+import nieve from './imagenes/nieve.jpg';
+import nublado from './imagenes/nublado.webp';
+import pocasnubes from './imagenes/pocas nubes.jpg'
+import tormenta from './imagenes/tormenta.jpg'
+export default[
+    {"img1":despejado,
+    "title":"despejado"},
+    {"img2":llovizna},
+    {"img3":lluviadelluvia},
+    {"img4":lluvia},
+    {"img5":niebla},
+    {"img6":nieve},
+    {"img7":nublado},
+    {"img8":pocasnubes},
+    {"img9":tormenta},
+
+]
